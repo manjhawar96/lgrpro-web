336175c0309903764a43be6f702f39ba4181f6e4
Nick Zhang <nzhang.jiahui@gmail.com>
2021-03-31 22:12:32 +0800

    :sparkles: Add responsive design

--- web/src/components/TeamPage/index.js
+++ web/src/components/TeamPage/index.js
@@ -4,14 +4,16 @@ import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import imgmj from './mj.jpeg'; 
 import imgex from './ex.jpeg';
+import imgnick from './nick.png';
+import './TeamPage.css';
 
 function TeamPage() {
     return (
         <Container className="slider-page d-flex container-fluid justify-content-center align-items-center"> 
-            <Row className="d-flex justify-content-between align-items-center">
-                <Col>
-                    <Card style={{ width: '15rem', border: 'none'}}>
-                        <Card.Img className="right-offset" variant="top" src={imgex} />
+            <Row className="d-flex justify-content-between align-items-center flex-column flex-md-row flex-lg-row flex-xl-row">
+                <Col className="col-4">
+                    <Card style={{ width: '100%', border: 'none'}}>
+                        <Card.Img className="right-offset team-member__image" variant="top" src={imgex} />
                         <Card.Body>
                             <Card.Title><a href="https://www.linkedin.com/in/emma-xia/">Emma Xia</a></Card.Title>
                             <Card.Text>
@@ -20,9 +22,9 @@ function TeamPage() {
                         </Card.Body>
                     </Card>
                 </Col>
-                <Col>
-                    <Card style={{ width: '15rem', border: 'none'}}>
-                        <Card.Img className="right-offset" variant="top" src={imgmj} />
+                <Col className="col-4">
+                    <Card style={{ width: '100%', border: 'none'}}>
+                        <Card.Img className="right-offset team-member__image" variant="top" src={imgmj} />
                         <Card.Body>
                             <Card.Title><a href="https://www.linkedin.com/in/manesh-jhawar-b07035125/">Manesh Jhawar</a></Card.Title>
                             <Card.Text>
@@ -31,10 +33,9 @@ function TeamPage() {
                         </Card.Body>
                     </Card>
                 </Col>
-                {/* 
-                <Col>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                <Col className="col-4">
+                    <Card style={{ width: '100%', border: 'none' }}>
+                        <Card.Img className="right-offset team-member__image" variant="top" src={imgnick} />
                         <Card.Body>
                             <Card.Title>Nick Zhang</Card.Title>
                             <Card.Text>
@@ -43,7 +44,7 @@ function TeamPage() {
                         </Card.Body>
                     </Card>
                 </Col>
-                */}
+               
             </Row>
         </Container>
     ); 
